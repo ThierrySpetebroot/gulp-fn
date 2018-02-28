673010fa44b7428019d5f67766adf3f3b26870ab
tspetebr <thierry.spetebroot@gmail.com>
2018-02-28 18:21:58 +0100

    fixed gulpfile.js, simplified count task and added hello_file task)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
-var es = require('event-stream');
+#!/usr/bin/env node
+const path = require('path');
+const es = require('event-stream');
 
-var gulp = require('gulp');
-var gulpFn = require('./gulp-fn');
+const gulp = require('gulp');
+const gulpFn = require('./gulp-fn');
 
 gulp.task('count', function() {
 	var actual = 0;
-	var streams = [];
 
-	streams.push(
-		gulp.src('./gulp-fn.js')
-		  .pipe(gulpFn(function(){actual++;}));
-	);
-	streams.push(
-		gulp.src('*')
-		  .pipe(gulpFn(function(){console.log(actual);}));
-	);
+	return gulp.src('*.js')
+		.pipe(gulpFn(function(){ actual++; }))
+		.pipe(es.wait(function(){ console.log(actual); }))
+});
 
-	return es.merge.apply(this, streams);
+gulp.task('hello_file', function() {
+	return gulp.src(['**/*', '!node_modules{,/**}'])
+		.pipe(gulpFn(function(file) {
+			console.log(path.basename(file.path));
+		}))
+		.pipe(es.wait(function(){ console.log("END"); }));
 });
