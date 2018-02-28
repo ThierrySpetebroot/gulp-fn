a4db34a1707c2ce4a6882975fc69331ea07b6a5c
tspetebr <thierry.spetebroot@gmail.com>
2018-02-28 18:27:36 +0100

    fixed tests, added package-lock.json and devDependencies

--- spec/gulp-fn.spec.js
+++ spec/gulp-fn.spec.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 var es = require('event-stream');
 
 var gulp = require('gulp')
@@ -5,23 +7,27 @@ var gulpFn = require('./../gulp-fn.js');
 
 describe('gulp-fn', function() {
 	it('should count the correct number of files if the passed function is a counter', function() {
-		var expected = 1;
+		var expected = 2;
 		var actual = 0;
 
-		gulp.src('./../gulp-fn.js')
+		gulp.src('*.js')
 		  .pipe(gulpFn(function(){actual++;}))
 		  .pipe(es.wait(function(){
-			expect(expected).toBe(actual);
+				expect(expected).toBe(actual);
 		  }));
 	});
 	it('should collect the correct number of files if the passed function saves all the file names in the pipe', function() {
-		var expected = ['gulp-fn.js'];
+		var expected = ['gulp-fn.js', 'gulpfile.js'];
 		var actual = [];
 
-		gulp.src('./../gulp-fn.js')
-		  .pipe(gulpFn(function(file){actual.push(file.path);}))
+		gulp.src('*.js')
+		  .pipe(gulpFn(function(file){
+				actual.push(
+					path.basename(file.path)
+				);
+			}))
 		  .pipe(es.wait(function(){
-			expect(expected).toEqual(actual);
+				expect(expected).toEqual(actual);
 		  }));
 	});
 });
